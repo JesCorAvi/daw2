45ce5a98e5bc7c4dd9991026694ca97e837d8f93
JesCorAvi <jesuscorderoavila1@gmail.com>
2023-09-20 21:15:48 +0200

    Actividades de 20-7-2023

---
+++ dwese/hola.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php 
+        #esto se ejecuta
+        $x = 5;
+        $z = 45;
+        echo $x + $z;
+        unset($z)
+        ?>
+        
+    </p>
+</body>
+</html>
