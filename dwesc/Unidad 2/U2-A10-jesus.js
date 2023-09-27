b718a7850193f5331871bdc442fd5be0e031e828
JesCorAvi <jesuscorderoavila1@gmail.com>
2023-09-27 21:49:01 +0200

    m

--- dwesc/Unidad 2/U2-A10-jesus.js
+++ dwesc/Unidad 2/U2-A10-jesus.js
@@ -1,8 +1,8 @@
 function tablas() {    
     var n1 = parseInt(window.prompt("dime un numero"));
     var tabla  = "";
-    for(var n2 = 0; n2 < 10 ;  n2++){
-        tabla = tabla + n1 + " + " + n2, " = ", n1 * n2 +"<br>"
+    for(var n2 = 1; n2 <= 10 ;  n2++){
+        tabla = tabla + n1 + " x " + n2 + " = " + (n1 * n2) +"<br>"
     }
     document.getElementById("1").innerHTML = tabla;
 }
