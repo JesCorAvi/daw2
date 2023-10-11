31f51dbdb00d8d8148a8a367a5c131d8ed363d11
JesCorAvi <jesuscorderoavila1@gmail.com>
2023-10-11 19:39:48 +0200

    dww

---
+++ dwesc/Unidad 3/A01-U3-jesus.js
@@ -0,0 +1,8 @@
+function tiempo_res(){
+    var actual = new Date();
+    var año_nuevo = new Date(actual.getFullYear() + 1,0,1);
+    var intervalo = (año_nuevo.getTime() - actual.getTime()); 
+    var intervalo_dias = intervalo / (1000 * 60 * 60 * 24); 
+    document.write(`La fecha actual es ${actual}.<br>El año nuevo es en ${año_nuevo}.<br> Quedan ${Math.trunc(intervalo_dias)} dias o ${intervalo} milisegundos.`)
+};
+
