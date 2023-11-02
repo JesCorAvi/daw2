945f390abe106eba6c0b533548e97e5b71ac8c3f
JesCorAvi <jesuscorderoavila1@gmail.com>
2023-11-02 12:13:52 +0100

    m

--- dwesc/Unidad 3/A17-U3-jesus.js
+++ dwesc/Unidad 3/A17-U3-jesus.js
@@ -1,12 +1,11 @@
 function texto(){
     var patt = new RegExp(/^\s?[A-Z]\w*/);
-    var lineas = document.getElementById("texto").value.split(".");
-    console.log(lineas);
-    var cadena = "";
-    for(var x of lineas){
-        if (x != ""){
-            var cadena = cadena + x.replace(/[^a-zA-Z ]/,"").match(patt)[0] + " ";
+    var lineas = document.getElementById("texto").value.replace(/(\r\n\t|\n|\r|\t)/gm, "").split(".");
+    var palabras = "";
+    for(var frase of lineas){
+        if (frase != ""){
+            var palabras = palabras + frase.replace(/[^a-zA-Z ]/,"").match(patt)[0] + ", ";
         }
     }
-    document.getElementById("resultado").innerHTML = cadena
+    document.getElementById("resultado").innerHTML = palabras
 }
--- dwesc/Unidad 3/A20-U3-jesus.js
+++ dwesc/Unidad 3/A20-U3-jesus.js
@@ -1,3 +1,3 @@
 function ventana(){
-    window.open("https://www.google.com/", "Ventana con Google", "left=100,top=100,width=320,height=320")
+    var ventana = window.open("https://www.amazon.com/", "Ventana_con_amazon", "left=100,top=100,width=320,height=320")
 }
