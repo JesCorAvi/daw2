0c49936a4599e6afe899fd2ced8659f096a60e37
JesCorAvi <jesuscorderoavila1@gmail.com>
2023-11-07 21:15:25 +0100

    m

--- dwesc/Unidad 3/A19-U3-2-jesus.js
+++ dwesc/Unidad 3/A19-U3-2-jesus.js
@@ -1,10 +1,11 @@
 function colorWeb(){
-    var color = parent.marco1.document.getElementById("color").selectedIndex;
-    var Frame = parent.marco1.document.getElementById("marco").selectedIndex;
-        if(Frame == "0"){
-            window.parent.marco1.document.body.style.backgroundColor = color;
-        }
-        if(Frame == "1"){
-            window.parent.marco2.document.body.style.backgroundColor = color;
-        }
+    var iframe1 = parent.document.getElementById("marco1");
+    var color = iframe1.contentDocument.getElementById("color").value;
+    var frame = iframe1.contentDocument.getElementById("marcos").value;
+    if(frame == "0"){
+        parent.marco1.document.body.style.backgroundColor = color;
+    }
+    if(frame == "1"){
+        parent.marco2.document.body.style.backgroundColor = color;
+    }
 }
