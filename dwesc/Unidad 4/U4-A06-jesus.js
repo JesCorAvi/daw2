e13e1d5ca5dc19226d0c2986a176354da00fbd45
JesCorAvi <jesuscorderoavila1@gmail.com>
2023-11-14 15:25:18 +0100

    m

--- dwesc/Unidad 4/U4-A06-jesus.js
+++ dwesc/Unidad 4/U4-A06-jesus.js
@@ -1,10 +1,10 @@
 var nota = [];
 function notas(){
     for(let x = 0; x < 5; x++){
-        nota[x] = [prompt("Escriba el nombre de un alumno"),];
-        nota[x][1] = prompt("Escriba Su nota en Desarollo Web en Entorno Cliente");
-        nota[x][2] = prompt("Escriba Su nota en Desarollo Web en Entorno Servidor");
-        nota[x][3] = prompt("Escriba Su nota en Despliege de Aplicaciones Web");
+        nota[x] = [prompt("Escriba el nombre de un alumno:"),];
+        nota[x][1] = prompt("Escriba Su nota en Desarollo Web en Entorno Cliente:");
+        nota[x][2] = prompt("Escriba Su nota en Desarollo Web en Entorno Servidor: ");
+        nota[x][3] = prompt("Escriba Su nota en Despliege de Aplicaciones Web:");
     }
     console.table(nota);
     for(var registro = 0; registro < 5; registro++){ 
