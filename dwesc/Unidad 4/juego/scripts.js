527b6fbd46d9558eb26a0099411ce5bbf3c6c0df
JesCorAvi <jesuscorderoavila1@gmail.com>
2023-11-20 21:13:26 +0100

    actividades cliente

--- dwesc/Unidad 4/juego/scripts.js
+++ dwesc/Unidad 4/juego/scripts.js
@@ -1,11 +1,23 @@
 const nombre = document.getElementById('nombre').value;
 const clase = document.querySelector('select[name="clase"]').value;
-function crearPJ(nombre, clase){
-    this.nivel = 1;
+const armas = {
+        "Espada oxidada": 10, 
+        "Bastón de iniciado":10,
+        "Espada de soldado":30,
+        "Libro de hechizos":30
+};
+const armaduras = {
+        "Ropajes de aventurero": 10, 
+        "Ropajes reforzados":30
+};
+function crearPJ(nombre, clase, arma, armadura, nivel){
+    this.nivel = nivel;
     this.nombre = nombre;
     this.clase = clase;
-    this.equipamiento = {"proteccion":"Mediocre", "arma":"Mediocre","monedas":500, "pociones":0}
-}
+    this.equipamiento = {"arma":arma, "armadura":armadura, "monedas":500, "pociones":0};
+    this.estadisticas = {"fuerza":armas[arma] + (nivel * 5), "hp":50 + (nivel * 10), "experiencia":0 };
+};
+
 function mostrarMain(mainId) {
     var inicio = document.getElementById('inicio');
     var principal = document.getElementById('principal');
@@ -17,7 +29,30 @@ function mostrarMain(mainId) {
         principal.removeAttribute('hidden');
         inicio.setAttribute('hidden', true);
     }
-}
+};
 
-aventurero = new crearPJ(nombre, clase)
+protagonista = new crearPJ( nombre, 
+                            clase, 
+                            "Bandido Debil", 
+                            "Ropajes de aventurero", 
+                            1
+);
 
+bandidoDebil = new crearPJ( "Bandido Debil", 
+                            "guerrero", 
+                            "Espada oxidada", 
+                            "Ropajes de aventurero", 
+                            1
+);
+bandidoMedio = new crearPJ( "Bandido Medio", 
+                            "guerrero", 
+                            "Espada oxidada", 
+                            "Ropajes de aventurero", 
+                            3
+);
+bandidoFuerte = new crearPJ("Bandido Fuerte", 
+                            "guerrero", 
+                            "Espada de soldado", 
+                            "Ropajes reforzados", 
+                            3
+);
