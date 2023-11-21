f7fa47d9cb69295f099822238bb255c3c7f3b22d
JesCorAvi <jesuscorderoavila1@gmail.com>
2023-11-21 21:19:44 +0100

    pogreso juego

--- dwesc/Unidad 4/juego/scripts.js
+++ dwesc/Unidad 4/juego/scripts.js
@@ -1,5 +1,6 @@
 const nombre = document.getElementById('nombre').value;
 const clase = document.querySelector('select[name="clase"]').value;
+
 const armas = {
         "Espada oxidada": 10, 
         "Bastón de iniciado":10,
@@ -10,13 +11,64 @@ const armaduras = {
         "Ropajes de aventurero": 10, 
         "Ropajes reforzados":30
 };
-function crearPJ(nombre, clase, arma, armadura, nivel){
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+function personaje(nombre, clase, arma, armadura, nivel){
     this.nivel = nivel;
     this.nombre = nombre;
     this.clase = clase;
-    this.equipamiento = {"arma":arma, "armadura":armadura, "monedas":500, "pociones":0};
-    this.estadisticas = {"fuerza":armas[arma] + (nivel * 5), "hp":50 + (nivel * 10), "experiencia":0 };
-};
+    this.equip = {"arma":arma, "armadura":armadura, "monedas":500, "pociones":0};
+    this.stat = {"fuerza":armas[arma] + (nivel * 5), "hp":100 + (nivel * 10), "experiencia":0 };
+    this.curar = function(){
+        this.stat["hp"] = 100 + (nivel * 10);
+    }
+    this.combate = function(enemy){
+        while (this.stat["hp"] > 0|| enemy.stat["hp"] > 0){
+            let dañoEne = (enemy.stat["fuerza"] + getRandomInt(enemy.stat["fuerza"] / 1));
+            let dañoPers = (this.stat["fuerza"] + getRandomInt(this.stat["fuerza"] / 1));
+
+            this.stat["hp"] -= dañoEne
+            console.log(
+                `
+                ${this.nombre} 
+                ataca e inflinge ${dañoEne} puntos de daño a 
+                ${enemy.nombre}
+                `
+            )
+            if (enemy.stat["hp"] <= 0){
+                console.log(`${this.nombre} ha ganado el combate`)
+                this.stat.experiencia += (50 + enemy.stat["fuerza"])
+                console.log(
+                    `
+                    ${this.nombre} ha ganado ${(50 + enemy.stat["fuerza"])} puntos de experiencia
+                    `
+                )
+                if (this.stat.experiencia >= 200){
+                    this.nivel += 1
+                    this.stat.experiencia = 0;
+                    console.log(
+                        `
+                        ${this.nombre} ha subido a nivel ${this.nivel}
+                        `
+                    )
+                }
+                break
+            }
+            enemy.stat["hp"] -= dañoPers
+                console.log(
+                `
+                ${enemy.nombre} 
+                ataca e inflinge ${dañoPers} puntos de daño a 
+                ${this.nombre}
+                `)
+            if (this.stat["hp"] <= 0){
+                console.log(`${enemy.nombre} ha ganado el combate`)
+                break
+            }
+        }
+    }
+}
 
 function mostrarMain(mainId) {
     var inicio = document.getElementById('inicio');
@@ -31,28 +83,29 @@ function mostrarMain(mainId) {
     }
 };
 
-protagonista = new crearPJ( nombre, 
-                            clase, 
-                            "Bandido Debil", 
+protagonista = new personaje("Protagonista", 
+                            "Guerrero", 
+                            "Espada oxidada", 
                             "Ropajes de aventurero", 
-                            1
+                            3
 );
 
-bandidoDebil = new crearPJ( "Bandido Debil", 
+enemigoDebil = new personaje("Bandido Debil", 
                             "guerrero", 
                             "Espada oxidada", 
                             "Ropajes de aventurero", 
                             1
 );
-bandidoMedio = new crearPJ( "Bandido Medio", 
+enemigoMedio = new personaje("Bandido Medio", 
                             "guerrero", 
                             "Espada oxidada", 
                             "Ropajes de aventurero", 
                             3
 );
-bandidoFuerte = new crearPJ("Bandido Fuerte", 
+enemigoFuerte = new personaje("Bandido Fuerte", 
                             "guerrero", 
                             "Espada de soldado", 
                             "Ropajes reforzados", 
                             3
 );
+protagonista.combate(enemigoDebil)
