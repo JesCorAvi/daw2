67c2a1f443da2205aae1645a07e8a236fd64be21
JesCorAvi <jesuscorderoavila1@gmail.com>
2024-02-05 21:13:31 +0100

    tarea de js y diseño

---
+++ dwesc/Unidad 7/U7-A01.js
@@ -0,0 +1,9 @@
+var trabajadores
+//Tiene que abrirse en live server
+fetch('./U7-A01.json')
+    .then((response) => response.json())
+    .then((json) => document.write(
+        
+        )
+    );
+
