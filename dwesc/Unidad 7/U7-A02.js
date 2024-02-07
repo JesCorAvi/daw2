4406201d40773945d88de2912035f08df3aa185c
JesCorAvi <jesuscorderoavila1@gmail.com>
2024-02-07 20:34:22 +0100

    Borrada la actividad de rafa

--- dwesc/Unidad 7/U7-A02.js
+++
@@ -1,9 +0,0 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       document.getElementById("demo").innerHTML = xhttp.responseText;
-    }
-};
-xhttp.open("GET", "filename", true);
-xhttp.send();
