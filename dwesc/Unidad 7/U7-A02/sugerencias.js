b9a6f8e708f115662ee10320472e25cecf8a802b
JesCorAvi <jesuscorderoavila1@gmail.com>
2024-02-08 13:33:52 +0100

    Fetch

--- dwesc/Unidad 7/U7-A02/sugerencias.js
+++ dwesc/Unidad 7/U7-A02/sugerencias.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('ciudadInput').addEventListener('input', function() {
         var inputText = this.value;
@@ -23,6 +24,39 @@ document.addEventListener('DOMContentLoaded', function() {
             var li = document.createElement('li');
             li.textContent = ciudad;
             sugerenciasL.appendChild(li);
+        });
+    }
+});
+*/
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('ciudadInput').addEventListener('input', function() {
+        var inputText = this.value;
+        if (inputText.length > 0) {
+            fetch('ciudadessugerencias.php?input=' + inputText)
+                .then(function(response) {
+                    if (!response.ok) {
+                        throw new Error('Error en la respuesta de la solicitud.');
+                    }
+                    return response.json();
+                })
+                .then(function(data) {
+                    mostrarSugerencias(data);
+                })
+                .catch(function(error) {
+                    console.error('Error en la solicitud:', error);
+                });
+        } else {
+            document.getElementById('sugerencias').innerHTML = '';
+        }
+    });
+
+    function mostrarSugerencias(sugerencias) {
+        var sugerenciasList = document.getElementById('sugerencias');
+        sugerenciasList.innerHTML = '';
+        sugerencias.forEach(function(ciudad) {
+            var li = document.createElement('li');
+            li.textContent = ciudad;
+            sugerenciasList.appendChild(li);
         });
     }
 });
