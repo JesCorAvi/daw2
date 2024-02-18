6fcd002536d1129607174f8051a60164db1b91e5
JesCorAvi <jesuscorderoavila1@gmail.com>
2024-02-18 20:34:41 +0100

    Reactfa

--- dwesc/Unidad 8/U8A02/src/App.jsx
+++ dwesc/Unidad 8/U8A02/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import React, { useState, useEffect } from 'react';
 function App() {
-  const [count, setCount] = useState(0)
+	const [estado, setEstado] = useState(1);
+	const [valorInput, setValorInput] = useState(0);
+	const [restante, setRestante] = useState(0);
 
-  return (
+	useEffect(() => {
+		if (estado === 2) {
+			setRestante(parseInt(valorInput));
+			var tiempo = setInterval(() => {
+				setRestante(prevRestante => {
+					if (prevRestante === 0) {
+						clearInterval(tiempo);
+						siguiente()
+						return prevRestante;
+					}
+					return prevRestante - 1;
+				});
+			}, 1000);
+			
+			return () => clearInterval(tiempo);
+			
+		}
+	}, [estado, valorInput]);
+
+	function siguiente(){
+		setEstado(estado + 1)
+	}
+	return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+	{estado === 1 ? (
+
+    	<div className="card" >
+    		<p>
+        		¿Cuantos segundos vamos a contar?
+			</p>	
+			<input type="number" id="tiempo" onChange={(e) => setValorInput(e.target.value)}/>
+			<button id="contar" onClick={siguiente}>Contar</button>
+      	</div>
+
+	) : estado === 2 ? (
+
+		<div className="card">
+			{`Soy un conteo hasta el ${valorInput} y han transcurrido ${restante} segundos`}
+      	</div>
+
+	) : (
+
+    	<div className="card">
+    		<p>
+        		SE HA TERMINADO EL TIEMPO
+			</p>
+			<button id="reiniciar" onClick={() => setEstado(1)}>REINICIAR</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+	)
+	}
     </>
-  )
+  	)
 }
+
 
 export default App
